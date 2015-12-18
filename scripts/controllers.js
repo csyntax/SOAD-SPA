6719e2bf0dd9715187a67efba6b6c1b85ab0f87a
csyntax <csyntax@outlook.com>
2015-12-18 20:39:43 +0200

    add filters

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -1,26 +1,11 @@
-app.controller("HomeController", function ($scope) {
-	
-});
-
 app.controller("AlbumsController", function ($scope, Albums) {
 	Albums.getAlbums(function (albums) {
         $scope.albums = albums;
     });
-
-	/*$http.get("/data/data.json").then(function (response) {
-		$scope.albums = response.data.albums;
-	});*/
 });
 
 app.controller("AlbumController", function ($scope, $routeParams, Albums) {
-	/*$http.get("/data/data.json").then(function (response) {
-		var id = $routeParams.id;
-
-		$scope.album = Albums.getAlbumById(id);
-	});*/
-
 	Albums.getAlbum($routeParams.id, function (album) {
 		 $scope.album = album;
 	});
-	
 });
