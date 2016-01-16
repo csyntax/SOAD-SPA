bb1a1e602e5eaa65a50e700a162689a8e2695024
csyntax <csyntax@outlook.com>
2016-01-16 18:38:11 +0200

    update node app service

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
 
 var app = express();
 
-var env = process.env.NODE_ENV || 'development';
+var env = process.env.NODE_ENV || "development";
 app.locals.ENV = env;
-app.locals.ENV_DEVELOPMENT = env == 'development';
+app.locals.ENV_DEVELOPMENT = env == "development";
 
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("port", process.env.PORT || 3000);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "views", "index.html"));
+	res.render("index");
 });
 
-app.listen(app.get('port'), function() {
-    console.log('Express server listening on port ' + app.get("port"));
+app.listen(app.get("port"), function () {
+	console.log("Express server listening on port " + app.get("port"));
 });
